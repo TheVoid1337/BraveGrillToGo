888722ff5849c9533f0d1ca4cc3be8c73a9121d1
TheVoid1337 <just.biemek@gmail.com>
2024-03-19 17:22:32 +0100

    add(application settings)

---
+++ src/main/java/com/bravegrill/bravegrilltogo/BraveGrillToGoApplication.java
@@ -0,0 +1,13 @@
+package com.bravegrill.bravegrilltogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BraveGrillToGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BraveGrillToGoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bravegrill/bravegrilltogo/BraveGrillToGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bravegrill.bravegrilltogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BraveGrillToGoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
