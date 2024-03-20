8e6efbfba36664721713b809903e7abbc9df0971
TheVoid1337 <just.biemek@gmail.com>
2024-03-20 18:08:27 +0100

    edit(equals by id)

--- src/main/java/entity/Product.java
+++ src/main/java/entity/Product.java
@@ -5,6 +5,7 @@ import exceptions.InvalidProductException;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import valueObjects.Price;
 
 @Entity
@@ -14,6 +15,7 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Getter
+    @Setter
     private Integer productID;
 
     @Getter
@@ -23,10 +25,17 @@ public class Product {
     @Embedded
     private Price price;
 
-    public Product(int productID, float price){
+    public Product(int productID, float price, String description){
         if (productID<=0)
             throw new InvalidProductException("Invalid product ID");
+        if (description == null)
+            throw new InvalidProductException("Description for product can not be null!");
+        if (description.isBlank() || description.length() < 3)
+            throw new InvalidProductException("Product description must contain at least 3 letters");
+
         this.price = new Price(price);
+        this.productID = productID;
+        this.description = description;
     }
 
     public Product(float price, String description){
@@ -54,5 +63,14 @@ public class Product {
 
     public void setPrice(float price){
         this.price = new Price(price);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj.getClass().equals(Product.class)){
+            return productID.equals(((Product) obj).getProductID());
+        }else {
+            return false;
+        }
     }
 }
