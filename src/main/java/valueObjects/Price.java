06ffa2d249658b692c7b77471cf91d344c66fd2c
TheVoid1337 <just.biemek@gmail.com>
2024-03-20 16:11:18 +0100

    add(price for product + tests + exceptions)

---
+++ src/main/java/valueObjects/Price.java
@@ -0,0 +1,23 @@
+package valueObjects;
+
+import exceptions.InvalidPriceException;
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+public class Price {
+
+    private float price;
+
+
+    public Price(float price) {
+        if (price<=0)
+            throw new InvalidPriceException("Price of product cannot be negative or zero!");
+        this.price = Float.valueOf(price);
+    }
+
+
+}
---
+++ src/test/java/valueObjects/PriceTest.java
@@ -0,0 +1,25 @@
+package valueObjects;
+
+import exceptions.InvalidPriceException;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest(classes = Price.class)
+class PriceTest {
+
+    @Test
+    void createPriceIncorrectTest(){
+        assertThrows(InvalidPriceException.class, ()->new Price(-10.0f));
+        assertThrows(InvalidPriceException.class, ()->new Price(-0.0f));
+    }
+
+    @Test
+    void createPriceCorrectTest(){
+        Price price = new Price(3.44f);
+        assertEquals(3.44f, price.getPrice());
+    }
+
+}
